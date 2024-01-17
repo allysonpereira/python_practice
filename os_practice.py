5b6a07073b732a5bb2136c37aceef8bc59dbb17e
Allyson <113621581+allysonpereira@users.noreply.github.com>
2024-01-17 15:57:51 -0500

    Add files via upload

---
+++ os_practice.py
@@ -0,0 +1,41 @@
+import os
+from datetime import datetime
+
+#print(os.getcwd())
+
+#os.chdir('C:/Users/c000851700/Desktop/Allyson')
+
+#print(os.getcwd())
+
+os.chdir('C:/Users/c000851700/Desktop/Allyson')
+
+#file_path = os.path.join(os.environ.get('HOME'), 'practice.py')
+
+os.mkdir('Kubernetes')
+
+#print(os.listdir())
+
+#os.mkdir('jan11.2023')
+#os.makedir('jan11.2023/d)
+
+#os.rmdir('jan11.2023')
+#os.removedirs()
+
+#os.rename('os.txt' , 'os_practice.txt')  #original name file and new name
+
+#print(os.stat('os_practice.txt').st_size)
+
+#modtime = os.stat('os_practice.txt').st_mtime
+#print(datetime.fromtimestamp(modtime))
+
+#for dirpath, dirnames, filenames in os.walk('C:/Users/c000851700/Desktop/'):
+ #   print('Current Path:' , dirpath)
+  #  print('Directories:' , dirnames)
+   # print('Files:' , filenames)
+    #print()
+
+
+
+
+#print(os.listdir())
+
